8848357088926acdf21a15d4199e60b5fbf3bc9f
cyber-mat <olaniyanolamide@gmail.com>
2020-05-28 14:03:56 +0100

    Almost done, state variable.

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-let playerScore, computerScore, selection, comp;
-playerScore = 0;
-computerScore = 0;
+let playerScore, computerScore, selection, gamePlaying, comp, winningScore;
 
 newGame();
+
+function newGame() {
+    playerScore = 0;
+    computerScore = 0;
+    winningScore = 10;
+
+    document.querySelector('.player-selection').textContent = '';
+    document.querySelector('.computer-selection').textContent = '';
+    document.querySelector('.player-hand').style.display = 'none';
+    document.querySelector('.computer-hand').style.display = 'none';
+    document.querySelector('.player-score').textContent = '0';
+    document.querySelector('.computer-score').textContent = '0';
+
+    document.querySelector('.player-name').textContent = 'Player';
+    document.querySelector('.player-panel').classList.remove('winner');
+    document.querySelector('.computer-name').textContent = 'Computer';
+    document.querySelector('.computer-panel').classList.remove('winner');
+
+    gamePlaying = true;
+
+}
+
+function getWinner() {
+    if (playerScore >= winningScore || computerScore >= winningScore) {
+        if (playerScore >= winningScore) {
+            document.querySelector('.player-name').textContent = 'Winner!';
+            document.querySelector('.player-panel').classList.add('winner');
+
+        } else {
+            document.querySelector('.computer-name').textContent = 'Winner!';
+            document.querySelector('.computer-panel').classList.add('winner');
+        }
+        gamePlaying = false;
+    }
+}
 
 function computerPlay() {
     comp = Math.floor((Math.random() * 3) + 1);
@@ -19,77 +52,78 @@ function computerPlay() {
 }
 
 function rock() {
-    document.querySelector('.player-selection').textContent = 'Rock';
-    document.querySelector('.player-hand').style.display = 'block';
-    document.querySelector('.player-hand').setAttribute('src', 'rock.png');
+    if (gamePlaying === true) {
+        document.querySelector('.player-selection').textContent = 'Rock';
+        document.querySelector('.player-hand').style.display = 'block';
+        document.querySelector('.player-hand').setAttribute('src', 'rock.png');
 
-    computerPlay();
+        computerPlay();
 
-    if (comp === 1) {
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else if (comp === 2) {
-        playerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else {
-        computerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
+        if (comp === 1) {
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else if (comp === 2) {
+            playerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else {
+            computerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        };
     };
-
+    getWinner();
 }
 
 function paper() {
-    document.querySelector('.player-selection').textContent = 'Paper';
-    document.querySelector('.player-hand').style.display = 'block';
-    document.querySelector('.player-hand').setAttribute('src', 'paper.png');
+    if (gamePlaying === true) {
+        document.querySelector('.player-selection').textContent = 'Paper';
+        document.querySelector('.player-hand').style.display = 'block';
+        document.querySelector('.player-hand').setAttribute('src', 'paper.png');
 
-    computerPlay();
+        computerPlay();
 
-    if (comp === 1) {
-        playerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else if (comp === 2) {
-        computerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else {
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
+        if (comp === 1) {
+            playerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else if (comp === 2) {
+            computerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else {
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        };
     };
+    getWinner();
 }
 
 function scissors() {
-    document.querySelector('.player-selection').textContent = 'Scissors';
-    document.querySelector('.player-hand').style.display = 'block';
-    document.querySelector('.player-hand').setAttribute('src', 'scissors.png');
+    if (gamePlaying === true) {
 
-    computerPlay();
+        document.querySelector('.player-selection').textContent = 'Scissors';
+        document.querySelector('.player-hand').style.display = 'block';
+        document.querySelector('.player-hand').setAttribute('src', 'scissors.png');
 
-    if (comp === 1) {
-        computerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else if (comp === 2) {
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    } else {
-        playerScore++;
-        document.querySelector('.player-score').textContent = playerScore;
-        document.querySelector('.computer-score').textContent = computerScore;
-    };
+        computerPlay();
+
+        if (comp === 1) {
+            computerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else if (comp === 2) {
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        } else {
+            playerScore++;
+            document.querySelector('.player-score').textContent = playerScore;
+            document.querySelector('.computer-score').textContent = computerScore;
+        };
+    }
+    getWinner();
 }
 
-function newGame() {
-    document.querySelector('.player-selection').textContent = '';
-    document.querySelector('.computer-selection').textContent = '';
-    document.querySelector('.player-hand').style.display = 'none';
-    document.querySelector('.computer-hand').style.display = 'none';
-    document.querySelector('.player-score').textContent = '0';
-    document.querySelector('.computer-score').textContent = '0';
-}
 
 document.querySelector('.btn-rock').addEventListener('click', rock);
 document.querySelector('.btn-paper').addEventListener('click', paper);
